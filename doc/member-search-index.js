c01efe0ae99fc5880e08c8932b5226d93f9333a1
Bocco1 <jacopo.bocco@gmail.com>
2022-05-07 12:15:07 +0200

    verisione 0

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Trapezio","l":"main(String[])","url":"main-java.lang.String:A-"},{"p":"<Unnamed>","c":"Trapezio","l":"Trapezio(float, float, float)"}]
---
+++ src/Trapezio.java
@@ -0,0 +1,57 @@
+/**
+ * <U> Trapezio </U>
+ * 
+ * <UL>
+ *   <li> Calcolo Area </li>
+ *   <li> Main </li>
+ * </UL>
+ * 
+ * @author <h1>BoccoJacopo</h1>
+ * @version 1.0
+ *
+ */
+public class Trapezio 
+{
+	
+	private float B,h,b;
+	
+	/**
+	 * 
+	 * @param B base maggiore 
+	 * @param b base minore
+	 * @param h altezza
+	 */
+	public Trapezio(float b, float h, float B) 
+	{	
+		this.B = B;
+		this.h = h;
+		this.b = b;
+	}
+
+	/**
+	 * 
+	 * 
+	 * @return area ritorna area del trapezio
+	 */
+	private float calcolaArea()
+	{
+		float area;
+		area = B+b*h/2;
+		return area;
+	}
+	
+	public static void main(String[] args) 
+	{
+		Trapezio trap = new Trapezio(4, 5, 2);
+		Trapezio trap1 = new Trapezio(5, 6, 7);
+		Trapezio trap2 = new Trapezio(7, 8, 9);
+		
+	    System.out.println(trap.calcolaArea());
+	    System.out.println(trap1.calcolaArea());
+	    System.out.println(trap2.calcolaArea());
+		
+		
+	}
+	
+
+}
